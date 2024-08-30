4470c0e6bda292fdb1587808f176b003c216098c
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-08-29 23:01:30 -0600

    dishe y service

--- src/domain/ServiceRequest.java
+++ src/domain/ServiceRequest.java
@@ -1,9 +1,40 @@
 package domain;
 
+import java.util.Arrays;
+
 public class ServiceRequest {
 
-	public ServiceRequest() {
-		// TODO Auto-generated constructor stub
+	private Student student;
+	private Dishe[] dishe;
+	
+	public ServiceRequest() {}
+
+	public ServiceRequest(Student student, Dishe[] dishe) {
+		super();
+		this.student = student;
+		this.dishe = dishe;
 	}
 
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Dishe[] getDishe() {
+		return dishe;
+	}
+
+	public void setDishe(Dishe[] dishe) {
+		this.dishe = dishe;
+	}
+
+	@Override
+	public String toString() {
+		return   student + "-" + Arrays.toString(dishe);
+	}
+
+	
 }
