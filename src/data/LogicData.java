7cd9222afb145312ad9cbdd77c6c6a135e6c0d1b
KeynaCaFu <keynacastrofuentes@gmail.com>
2024-08-30 13:15:57 -0600

    logic

--- src/data/LogicData.java
+++ src/data/LogicData.java
@@ -1,89 +1,35 @@
 package data;
 
+import domain.Dishe;
+
 public class LogicData {
 
 	public LogicData() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void updateDishes(boolean serviceHours, String serviceDay) {
+	public void deleteDishes(Dishe d,boolean serviceHours, String serviceDay) {
 		
 		if(serviceHours == true && serviceDay.equals("Lunes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Lunes")) {
-			
+			DisheData.deleteMonday_Lunch(d);
+		}else if(serviceHours == false && serviceDay.equals("Lunes")) {// desayuno
+			DisheData.deleteMonday_Breakfast(d);
 		}else if(serviceHours == true && serviceDay.equals("Martes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Martes")) {
-			
+			DisheData.deleteTuesday_Lunch(d);
+		}else if(serviceHours == false && serviceDay.equals("Martes")) {// desayuno
+			DisheData.deleteTuesday_Breakfast(d);
 		}else if(serviceHours == true && serviceDay.equals("Miercoles")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Miercoles")) {
-			
+			DisheData.deleteWednesday_Lunch(d);
+		}else if(serviceHours == false && serviceDay.equals("Miercoles")) {// desayuno
+			DisheData.deleteWebnesday_Breakfast(d);
 		}else if(serviceHours == true && serviceDay.equals("Jueves")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Jueves")) {
-			
+			DisheData.deleteThursday_Lunch(d);
+		}else if(serviceHours == false && serviceDay.equals("Jueves")) {// desayuno
+			DisheData.deleteThursday_Breakfast(d);
 		}else if(serviceHours == true && serviceDay.equals("Viernes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Viernes")) {
-			
-		}else {
-			
-		}
-		
-	}
-	
-	public void saveDishes(boolean serviceHours, String serviceDay) {
-		
-		if(serviceHours == true && serviceDay.equals("Lunes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Lunes")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Martes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Martes")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Miercoles")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Miercoles")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Jueves")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Jueves")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Viernes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Viernes")) {
-			
-		}else {
-			
-		}
-		
-	}
-	
-	public void deleteDishes(boolean serviceHours, String serviceDay) {
-		
-		if(serviceHours == true && serviceDay.equals("Lunes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Lunes")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Martes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Martes")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Miercoles")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Miercoles")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Jueves")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Jueves")) {
-			
-		}else if(serviceHours == true && serviceDay.equals("Viernes")) {
-			
-		}else if(serviceHours == false && serviceDay.equals("Viernes")) {
-			
+			DisheData.deleteFriday_Lunch(d);
+		}else if(serviceHours == false && serviceDay.equals("Viernes")) {// desayuno
+			DisheData.deleteFriday_Breakfast(d);
 		}else {
 			
 		}
