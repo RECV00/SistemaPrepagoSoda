8ad5e3f9795d1e685e1a57773d1186a1077031cb
RECV00 <corralesreychell@gmail.com>
2024-11-02 13:51:41 -0600

    Update ServerConnection.java

--- src/data/ServerConnection.java
+++ src/data/ServerConnection.java
@@ -24,7 +24,7 @@ public class ServerConnection {
     }
     public void startServer() {
         try {
-            serverSocket = new ServerSocket(12348); // Puerto del servidor
+            serverSocket = new ServerSocket(12349); // Puerto del servidor
             System.out.println("Servidor iniciado, esperando conexiones...");
 
             while (true) {
